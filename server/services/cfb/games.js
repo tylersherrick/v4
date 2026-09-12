d43f7c07705d3fa20438637d39312a0d2cb250a5
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-09-11 19:37:09 -0500

    Add live possession data to CFB games

--- server/services/cfb/games.js
+++ server/services/cfb/games.js
@@ -37,6 +37,7 @@ export async function getGames(season, week) {
         completed: event.status?.type?.completed,
       },
       venue: competition?.venue?.fullName,
+      possession: competition?.situation?.possession || null,
       awayTeam: {
         id: awayTeam?.team?.id,
         name: awayTeam?.team?.displayName,
