0d89a51b8a31c3b37859eeff6d6ebe3f11e1789a
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-09-19 09:36:02 -0500

    Organize CFB rosters by team unit
    Group CFB roster players into offense, defense, and special teams using ESPN position data while preserving player profile details for the team roster interface.

--- server/services/cfb/roster.js
+++ server/services/cfb/roster.js
@@ -1,6 +1,55 @@
 const ESPN_URL =
   "https://site.api.espn.com/apis/site/v2/sports/football/college-football/teams";
 
+const OFFENSE_POSITIONS = [
+  "QB",
+  "RB",
+  "FB",
+  "WR",
+  "TE",
+  "OL",
+  "OT",
+  "OG",
+  "C",
+];
+
+const DEFENSE_POSITIONS = [
+  "DL",
+  "DE",
+  "DT",
+  "LB",
+  "ILB",
+  "OLB",
+  "DB",
+  "CB",
+  "S",
+];
+
+const SPECIAL_TEAMS_POSITIONS = [
+  "PK",
+  "K",
+  "P",
+  "LS",
+];
+
+function formatPlayer(player) {
+  return {
+    id: player.id,
+    name: player.displayName,
+    firstName: player.firstName || null,
+    lastName: player.lastName || null,
+    jersey: player.jersey || null,
+    position: player.position?.abbreviation || null,
+    positionName:
+      player.position?.displayName || null,
+    class:
+      player.experience?.displayValue || null,
+    height: player.displayHeight || null,
+    weight: player.displayWeight || null,
+    headshot: player.headshot?.href || null,
+  };
+}
+
 export async function getRoster(teamId) {
   const response = await fetch(`${ESPN_URL}/${teamId}/roster`);
 
@@ -12,6 +61,23 @@ export async function getRoster(teamId) {
 
   const data = await response.json();
 
+  const players =
+    data.athletes?.flatMap((group) =>
+      (group.items || []).map(formatPlayer)
+    ) || [];
+
+  const offense = players.filter((player) =>
+    OFFENSE_POSITIONS.includes(player.position)
+  );
+
+  const defense = players.filter((player) =>
+    DEFENSE_POSITIONS.includes(player.position)
+  );
+
+  const specialTeams = players.filter((player) =>
+    SPECIAL_TEAMS_POSITIONS.includes(player.position)
+  );
+
   return {
     team: {
       id: data.team?.id || teamId,
@@ -22,21 +88,8 @@ export async function getRoster(teamId) {
         data.team?.logos?.[0]?.href ||
         null,
     },
-    players:
-      data.athletes?.flatMap((group) =>
-        (group.items || []).map((player) => ({
-          id: player.id,
-          name: player.displayName,
-          firstName: player.firstName || null,
-          lastName: player.lastName || null,
-          jersey: player.jersey || null,
-          position: player.position?.abbreviation || null,
-          positionName: player.position?.displayName || null,
-          class: player.experience?.displayValue || null,
-          height: player.displayHeight || null,
-          weight: player.displayWeight || null,
-          headshot: player.headshot?.href || null,
-        }))
-      ) || [],
+    offense,
+    defense,
+    specialTeams,
   };
 }
